75bb1a3e9d45159daa7a8ce5db8b50a7a2e8e452
hanbingi <138805334+hanbingi@users.noreply.github.com>
2024-03-22 21:11:56 +0800

    main.js

--- js/main.js
+++ js/main.js
@@ -313,7 +313,7 @@ $("#bilibili")
   });
 $("#telegram")
   .mouseover(function () {
-    $("#link-text").html("来看看吗?");
+    $("#link-text").html("我的其他账号");
   })
   .mouseout(function () {
     $("#link-text").html("通过这里联系我");
