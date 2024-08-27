ca4c3f18d902bf44839acf4cad6ba79e7a15f17e
hanbingi <167499304@qq.com>
2024-08-27 22:35:27 +0800

    Site updated: 2024-08-27 22:35:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/posts/10.html","/posts/5.html","/posts/12.html","/posts/6.html","/posts/9.html","/posts/1.html","/posts/2.html","/posts/3.html","/posts/11.html","/posts/7.html","/posts/13.html","/posts/4.html","/posts/8.html"];function toRandomPost(){
+var posts=["/posts/10.html","/posts/5.html","/posts/12.html","/posts/6.html","/posts/9.html","/posts/3.html","/posts/11.html","/posts/2.html","/posts/7.html","/posts/1.html","/posts/13.html","/posts/4.html","/posts/8.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
